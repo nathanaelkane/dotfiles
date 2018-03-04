205519c18beda45cb03c933f0da6814328de60b8
Nate Kane <nathanaelkane@gmail.com>
2018-03-05 09:34:38 +1000

    Add heroku autocomplete

---
+++ shell/config/heroku.sh
@@ -0,0 +1,2 @@
+# heroku autocomplete setup
+CLI_ENGINE_AC_BASH_SETUP_PATH=~/Library/Caches/heroku/completions/bash_setup && test -f $CLI_ENGINE_AC_BASH_SETUP_PATH && source $CLI_ENGINE_AC_BASH_SETUP_PATH;
