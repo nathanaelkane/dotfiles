38997427fceba4eef079633ba36631b1b85ae381
Nate Kane <nathanaelkane@gmail.com>
2021-10-16 23:20:33 +1000

    nvim: Add lualine plugin

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,16 @@
+local use = require("packer").use
+
+use {
+  "shadmansaleh/lualine.nvim",
+  requires = "kyazdani42/nvim-web-devicons",
+
+  config = function()
+    require("lualine").setup {
+      options = {
+        theme = "nightfox",
+        section_separators = "",
+        component_separators = "",
+      },
+    }
+  end,
+}
