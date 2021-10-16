ce6bbf5603866b0d0eb358b8ea40845b5b3e381b
Nate Kane <nathanaelkane@gmail.com>
2021-10-16 22:43:50 +1000

    nvim: Add treesitter plugin

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,50 @@
+local use = require("packer").use
+
+use {
+  "nvim-treesitter/nvim-treesitter",
+  run = ":TSUpdate",
+  requires = "nvim-treesitter/playground",
+
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = {
+        "bash",
+        "dockerfile",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "query",
+        "rst",
+        "ruby",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = false,
+      },
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = 'o',
+          toggle_hl_groups = 'i',
+          toggle_injected_languages = 't',
+          toggle_anonymous_nodes = 'a',
+          toggle_language_display = 'I',
+          focus_language = 'f',
+          unfocus_language = 'F',
+          update = 'R',
+          goto_node = '<cr>',
+          show_help = '?',
+        },
+      },
+    }
+  end,
+}
