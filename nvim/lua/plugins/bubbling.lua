5f30f5686e8d62e3ed3a7366ec3f6a16f1e3416d
Nate Kane <nathanaelkane@gmail.com>
2021-10-16 23:02:16 +1000

    nvim: Add bubbling plugins
    Move lines up and down and move function args left and right.

---
+++ nvim/lua/plugins/bubbling.lua
@@ -0,0 +1,31 @@
+local use = require("packer").use
+
+use {
+  "matze/vim-move",
+
+  setup = function()
+    vim.g.move_map_keys = 0
+  end,
+
+  config = function()
+    local map = require("utils").map
+
+    -- Use option-J/K to bubble lines up and down
+    map("n", "˚", "<Plug>MoveLineUp", {noremap = false})
+    map("n", "∆", "<Plug>MoveLineDown", {noremap = false})
+    map("v", "˚", "<Plug>MoveBlockUp", {noremap = false})
+    map("v", "∆", "<Plug>MoveBlockDown", {noremap = false})
+  end,
+}
+
+use {
+  "AndrewRadev/sideways.vim",
+
+  config = function()
+    local map = require("utils").map
+
+    -- Use option-H/L to bubble items (function arguments, etc) left and right
+    map("n", "˙", ":SidewaysLeft<CR>", {silent = true})
+    map("n", "¬", ":SidewaysRight<CR>", {silent = true})
+  end,
+}
