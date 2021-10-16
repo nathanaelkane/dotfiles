bf62a42f3c7361416faf33972814306846f78ea2
Nate Kane <nathanaelkane@gmail.com>
2021-10-16 22:48:02 +1000

    nvim: Add nightfox colorscheme

---
+++ nvim/lua/plugins/nightfox.lua
@@ -0,0 +1,28 @@
+local use = require("packer").use
+
+use {
+  "EdenEast/nightfox.nvim",
+
+  config = function()
+    local nightfox = require("nightfox")
+
+    nightfox.setup {
+      fox = "nightfox",
+      hlgroups = {
+        LineNr = {fg = "#526175", bg = "#111B24"},
+        CursorLineNr = {fg = "#9D79D6", bg = "#111B24"},
+        SignColumn = {bg = "#111B24"},
+        GitSignsAdd = {bg = "#111B24"},
+        GitSignsChange = {bg = "#111B24"},
+        GitSignsDelete = {bg = "#111B24"},
+        IndentBlanklineChar = {fg = "#393B44"},
+        DiagnosticSignError = {fg = "#C94F6D", bg = "#111B24"},
+        DiagnosticSignWarn = {fg = "#DBC074", bg = "#111B24"},
+        DiagnosticSignHint = {fg = "#63CDCF", bg = "#111B24"},
+        DiagnosticSignInfo = {fg = "#719CD6", bg = "#111B24"},
+      }
+    }
+
+    nightfox.load()
+  end,
+}
