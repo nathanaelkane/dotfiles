c3ff5a2ff496b59f9d8fcc26d5e7ada2b93cc1c7
Nate Kane <nathanaelkane@gmail.com>
2021-10-16 23:26:57 +1000

    nvim: Add rest plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,11 +6,21 @@ use {
   requires = "nvim-treesitter/playground",
 
   config = function()
+    local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_configs.http = {
+      install_info = {
+        url = "https://github.com/NTBBloodbath/tree-sitter-http",
+        files = {"src/parser.c"},
+        branch = "main",
+      },
+    }
+
     require("nvim-treesitter.configs").setup {
       ensure_installed = {
         "bash",
         "dockerfile",
         "html",
+        "http",
         "javascript",
         "json",
         "lua",
