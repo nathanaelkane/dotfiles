bf37f79b3c064bd26a177907a993031792bf1600
Nate Kane <nathanaelkane@gmail.com>
2021-10-16 23:14:17 +1000

    nvim: Add indent-blankline plugin

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,20 @@
+local use = require("packer").use
+
+use {
+  "lukas-reineke/indent-blankline.nvim",
+
+  config = function()
+    require("indent_blankline").setup {
+      char = "│",
+      show_first_indent_level = true,
+      show_trailing_blankline_indent = false,
+      filetype_exclude = {
+        "vimwiki",
+        "packer",
+      },
+      buftype_exclude = {
+        "terminal",
+      },
+    }
+  end,
+}
