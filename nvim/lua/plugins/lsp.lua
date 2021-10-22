7bc7b9bb811c5e342b3d7cf516dd486b741177ea
Nate Kane <nathanaelkane@gmail.com>
2021-10-22 21:38:29 +1000

    nvim: Add lsp-installer plugin

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -74,3 +74,24 @@ use {
     }
   end,
 }
+
+use {
+  "williamboman/nvim-lsp-installer",
+
+  config = function()
+    local lsp_installer = require("nvim-lsp-installer")
+
+    lsp_installer.on_server_ready(function(server)
+      local opts = {}
+
+      -- (optional) Customize the options passed to the server
+      -- if server.name == "tsserver" then
+      --   opts.root_dir = function() ... end
+      -- end
+
+      -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
+      server:setup(opts)
+      vim.cmd [[do User LspAttachBuffers]]
+    end)
+  end,
+}
