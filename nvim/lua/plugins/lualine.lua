954012ec6200be43eb09cc8f57b623c137638565
Nate Kane <nathanaelkane@gmail.com>
2022-02-10 09:46:57 +1000

    nvim: Tweak lualine plugin settings
    Remove git branch and show full file path.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -11,6 +11,22 @@ use {
         section_separators = "",
         component_separators = "",
       },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{"filename", path = 1}},
+        lualine_x = {"location"},
+        lualine_y = {},
+        lualine_z = {}
+      },
+      sections = {
+        lualine_a = {"mode" },
+        lualine_b = {"diagnostics"},
+        lualine_c = {{"filename", path = 1}},
+        lualine_x = {"encoding", "fileformat", "filetype"},
+        lualine_y = {"progress"},
+        lualine_z = {"location"}
+      },
     }
   end,
 }
