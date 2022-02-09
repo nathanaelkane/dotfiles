1303c1ab45add697f616cb9f300e8c514c8f958d
Nate Kane <nathanaelkane@gmail.com>
2022-02-09 21:53:20 +1000

    nvim: Redo configuration for cmp plugin

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -6,64 +6,34 @@ use {
   requires = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
     "saadparwaiz1/cmp_luasnip",
-    "onsails/lspkind-nvim",
   },
 
   config = function()
-    local has_words_before = function()
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
+    vim.cmd "set completeopt=menu,menuone,noselect"
 
-    local luasnip = require("luasnip")
     local cmp = require("cmp")
 
-    cmp.setup {
+    cmp.setup({
       snippet = {
         expand = function(args)
           require("luasnip").lsp_expand(args.body)
         end,
       },
       mapping = {
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm({select = true}),
-
-        -- supertab-like
-
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, {"i", "s"}),
-
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, {"i", "s"}),
+        ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
+        ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
       },
-      sources = {
-        {name = "nvim_lsp"},
+      sources = cmp.config.sources({
         {name = "buffer"},
+        {name = "nvim_lsp"},
         {name = "luasnip"},
-      },
-      formatting = {
-        format = require("lspkind").cmp_format({with_text = false, maxwidth = 50})
-      },
-    }
+      }),
+    })
   end,
 }
