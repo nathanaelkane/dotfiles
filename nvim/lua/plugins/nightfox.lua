8ca2bff5172dd35dcb7e9783b1e9a7bee051e035
Nate Kane <nathanaelkane@gmail.com>
2022-03-31 09:12:00 +1000

    nvim: Fix nightfox colorscheme config after major update
    Also reverted to default styles.

--- nvim/lua/plugins/nightfox.lua
+++ nvim/lua/plugins/nightfox.lua
@@ -4,30 +4,6 @@ use {
   "EdenEast/nightfox.nvim",
 
   config = function()
-    local nightfox = require("nightfox")
-
-    nightfox.setup {
-      fox = "nightfox",
-      hlgroups = {
-        LineNr = {fg = "#526175", bg = "#111B24"},
-        CursorLineNr = {fg = "#9D79D6", bg = "#111B24"},
-        SignColumn = {bg = "#111B24"},
-        GitSignsAdd = {bg = "#111B24"},
-        GitSignsChange = {bg = "#111B24"},
-        GitSignsDelete = {bg = "#111B24"},
-        IndentBlanklineChar = {fg = "#393B44"},
-        DiagnosticSignError = {fg = "#C94F6D", bg = "#111B24"},
-        DiagnosticSignWarn = {fg = "#DBC074", bg = "#111B24"},
-        DiagnosticSignHint = {fg = "#63CDCF", bg = "#111B24"},
-        DiagnosticSignInfo = {fg = "#719CD6", bg = "#111B24"},
-        HopNextKey = {fg = "#D67AD2"},
-        HopNextKey1 = {fg = "#9D79D6"},
-        HopNextKey2 = {fg = "#B8A1E3"},
-        Pmenu = {bg = "#111111"},
-        PmenuSel = {bg = "#000000"},
-      }
-    }
-
-    nightfox.load()
+    vim.cmd("colorscheme nightfox")
   end,
 }
