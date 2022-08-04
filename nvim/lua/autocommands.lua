1aa2453e36c147c8ad710b7b999a9a7f7fd78a5c
Nate Kane <nathanaelkane@gmail.com>
2022-08-04 16:03:46 +1000

    nvim: Turn on spell checking for git commits

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,2 +1,5 @@
 -- Add dashes to the list of 'word characters'
 vim.cmd [[autocmd Filetype * setlocal iskeyword+=-]]
+
+-- Turn on spell checking for git commits
+vim.cmd [[autocmd FileType gitcommit setlocal spell]]
