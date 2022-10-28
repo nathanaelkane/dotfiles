e1112421b117ef800279b788a90774783e81dfa3
Nate Kane <nathanaelkane@gmail.com>
2022-10-28 16:03:49 +1000

    nvim: Disable autosave message

--- nvim/lua/plugins/autosave.lua
+++ nvim/lua/plugins/autosave.lua
@@ -6,6 +6,11 @@ use {
   config = function()
     require("auto-save").setup {
       enabled = true,
+      execution_message = {
+        message = function()
+          return ""
+        end,
+      },
     }
   end,
 }
