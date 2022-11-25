488379e8fe7a8d03bfde6386a7f19b369a7f40c5
Nate Kane <nathanaelkane@gmail.com>
2022-11-25 15:58:32 +1000

    nvim: Highlight current context with indent guides

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -3,11 +3,42 @@ local use = require("packer").use
 use {
   "lukas-reineke/indent-blankline.nvim",
 
+  setup = function()
+    vim.g.indent_blankline_context_patterns = {
+      "class",
+      "^func",
+      "method",
+      "^if",
+      "while",
+      "for",
+      "with",
+      "try",
+      "except",
+      "arguments",
+      "argument_list",
+      "object",
+      "dictionary",
+      "element",
+      "table",
+      "tuple",
+      "do_block",
+      "when",
+      "else",
+      "pair",
+      "hash",
+      "module",
+      "assignment",
+      "call",
+    }
+  end,
+
   config = function()
     require("indent_blankline").setup {
       char = "│",
       show_first_indent_level = true,
       show_trailing_blankline_indent = false,
+      show_current_context = true,
+      show_current_context_start = true,
       filetype_exclude = {
         "packer",
         "neo-tree",
