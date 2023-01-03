6866f0173fe87026674aac69f3318767b55b2474
Nate Kane <nathanaelkane@gmail.com>
2023-01-03 21:03:17 +1000

    nvim: Update cmp config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -6,50 +6,26 @@ use {
   requires = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "saadparwaiz1/cmp_luasnip",
   },
 
   config = function()
     vim.cmd "set completeopt=menu,menuone,noselect"
 
     local cmp = require("cmp")
-    local cmp_buffer = require("cmp_buffer")
-
     cmp.setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
       mapping = {
         ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
         ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
+        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
+        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
       },
       sources = cmp.config.sources({
-        {
-          name = "buffer",
-          option = {
-            get_bufnrs = function()
-              return vim.api.nvim_list_bufs() -- all buffers
-            end,
-          },
-        },
         {name = "nvim_lsp"},
-        {name = "luasnip"},
+        {name = "buffer"},
       }),
-      sorting = {
-        comparators = {
-          function(...)
-            -- sort completion results based on the distance of the word from the cursor line
-            return cmp_buffer:compare_locality(...)
-          end,
-        },
-      },
     })
   end,
 }
