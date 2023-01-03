85e3dd66d69c476fe3298ab41d25705303554e46
Nate Kane <nathanaelkane@gmail.com>
2023-01-03 21:04:16 +1000

    nvim: Update neotest config

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -11,10 +11,21 @@ use {
   },
 
   config = function()
+    local map = require("utils").map
+
     require("neotest").setup({
       adapters = {
-        require("neotest-rspec"),
+        require("neotest-rspec")({
+          runner = "rspec",
+        }),
       },
     })
+
+    map("n", "<Leader>rr", ':lua require("neotest").run.run()<CR>') -- Run the nearest test
+    map("n", "<Leader>rf", ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>') -- Run the current file
+    map("n", "<Leader>rl", ':lua require("neotest").run.run_last()<CR>') -- Run the last test
+    map("n", "<Leader>ro", ':lua require("neotest").output.open()<CR>')
+    map("n", "<Leader>rO", ':lua require("neotest").output.open({short = true})<CR>')
+    map("n", "<Leader>rs", ':lua require("neotest").summary.toggle()<CR>')
   end,
 }
