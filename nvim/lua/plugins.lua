2d6639e38b42b9b414a53c7df98ab6082e49f455
Nate Kane <nathanaelkane@gmail.com>
2023-01-04 14:28:17 +1000

    nvim: Add summary comments for plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,8 +15,8 @@ if fn.empty(fn.glob(plenary_install_path)) > 0 then
 end
 
 require("packer").startup(function(use)
-  use "wbthomason/packer.nvim"
-  use "nvim-lua/plenary.nvim"
+  use "wbthomason/packer.nvim" -- plugin manager
+  use "nvim-lua/plenary.nvim" -- collection of helpful lua functions for NeoVim
 
   local plugin_dir = fn.stdpath("config") .. "/lua/plugins"
   local plugin_files = require("plenary.scandir").scan_dir(plugin_dir, {depth = 1})
@@ -26,8 +26,8 @@ require("packer").startup(function(use)
     require("plugins." .. plugin)
   end
 
-  use "AndrewRadev/linediff.vim"
-  use "wsdjeg/vim-fetch"
-  use "dstein64/nvim-scrollview"
-  use "godlygeek/tabular"
+  use "AndrewRadev/linediff.vim" -- diff two separate blocks of text
+  use "wsdjeg/vim-fetch" -- e.g. `nvim path/to/file.ext:12:3` will open file.ext on line 12 at column 3
+  use "dstein64/nvim-scrollview" -- display interactive vertical scrollbars
+  use "godlygeek/tabular" -- align text
 end)
