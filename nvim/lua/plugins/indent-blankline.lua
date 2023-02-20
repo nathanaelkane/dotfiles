14a12ae32450270f7df3a7965ffa452dae08fff1
Nate Kane <nathanaelkane@gmail.com>
2023-02-20 12:28:23 +1000

    nvim: Don't highlight current context with indent-blankline

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,41 +2,10 @@ return {
   "lukas-reineke/indent-blankline.nvim",
 
   config = function()
-    vim.g.indent_blankline_context_patterns = {
-      "class",
-      "^func",
-      "method",
-      "^if",
-      "while",
-      "for",
-      "with",
-      "try",
-      "except",
-      "arguments",
-      "argument_list",
-      "object",
-      "dictionary",
-      "element",
-      "table",
-      "tuple",
-      "do_block",
-
-      -- Ruby
-      "unless",
-      "when",
-      "else",
-      "pair",
-      "hash",
-      "module",
-      "assignment",
-      "call",
-    }
-
     require("indent_blankline").setup {
       char = "│",
       show_first_indent_level = true,
       show_trailing_blankline_indent = false,
-      show_current_context = true,
       filetype_exclude = {
         "packer",
         "neo-tree",
