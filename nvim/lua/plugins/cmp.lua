674df08a22c66a74947143ceff68cd19f5f45fa9
Nate Kane <nathanaelkane@gmail.com>
2023-04-25 22:30:16 +1000

    nvim: Reconfigure cmp and luasnip

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,25 +4,87 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
   },
 
   config = function()
-    vim.cmd "set completeopt=menu,menuone,noselect"
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
 
+    local luasnip = require("luasnip")
     local cmp = require("cmp")
+
     cmp.setup({
-      mapping = {
-        ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
-        ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
-        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
-        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
       },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+
+        -- enter triggers snippet (if applicable) or inserts newline, never selects result
+        ["<CR>"] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, {"i", "s"}),
+
+        -- SuperTab-like behaviour: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, {"i", "s"}),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {"i", "s"}),
+
+        -- use default ctrl+n/p behaviour when no words before cursor and completion popup not visible
+        -- handy for quickly completing the prev/next word without having to find it in completion results
+        ["<C-n>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, {"i", "c"}),
+        ["<C-p>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, {"i", "c"}),
+      }),
       sources = cmp.config.sources({
+        {name = "buffer", max_item_count = 5},
+        {name = "luasnip"},
         {name = "nvim_lsp"},
-        {name = "buffer"},
       }),
     })
   end,
--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,3 +1,11 @@
 return {
   "L3MON4D3/LuaSnip",
+  version = "<CurrentMajor>.*",
+  build = "make install_jsregexp",
+
+  config = function()
+    require("luasnip.loaders.from_lua").lazy_load({
+      paths = vim.fn.stdpath('config') .. '/snippets',
+    })
+  end,
 }
