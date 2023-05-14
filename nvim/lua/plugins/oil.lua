d1183fc9cda39a89e305f80f8536e1d917d3e724
Nate Kane <nathanaelkane@gmail.com>
2023-05-14 16:24:23 +1000

    nvim: Disable conceal for oil plugin

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,7 +3,12 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
 
   config = function()
-    require("oil").setup()
+    require("oil").setup {
+      win_options = {
+        conceallevel = 0,
+      },
+    }
+
     vim.keymap.set("n", "-", require("oil").open, {desc = "Open parent directory"})
   end,
 }
