0184a645183798e7d5ba42e1b5a36dae0cec9808
Nate Kane <nathanaelkane@gmail.com>
2023-05-14 16:26:08 +1000

    nvim: Remove nvim-scrollview plugin

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -5,9 +5,6 @@ return {
   -- e.g. `nvim path/to/file.ext:12:3` will open file.ext on line 12 at column 3
   "wsdjeg/vim-fetch",
 
-  -- display interactive vertical scrollbars
-  "dstein64/nvim-scrollview",
-
   -- align text
   "godlygeek/tabular",
 }
