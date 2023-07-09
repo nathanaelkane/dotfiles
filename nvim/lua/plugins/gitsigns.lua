593b5ae0731871e642c50ac92bb46af1dde646a5
Nate Kane <nathanaelkane@gmail.com>
2023-07-09 14:56:31 +1000

    nvim: config.keymaps has been removed from the gitsigns plugin

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -4,7 +4,6 @@ return {
 
   config = function()
     require("gitsigns").setup({
-      keymaps = {},
       signs = {
         add = {hl = 'GitSignsAdd' , text = '▎', numhl='GitSignsAddNr' , linehl='GitSignsAddLn'},
         change = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
