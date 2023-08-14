f3b5e53467f5c01f1bdd59b4fac9cbf565593851
Nate Kane <nathanaelkane@gmail.com>
2023-08-14 21:15:37 +1000

    nvim: Configure flash

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,5 +1,22 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    modes = {
+      search = {
+        enabled = false,
+      }
+    },
+  },
+
+  keys = {
+    {
+      "<leader>e",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+  },
 }
