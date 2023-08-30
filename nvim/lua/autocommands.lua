d631dad7b63eec47aa87ec9db3fe0771c2f1750b
Nate Kane <nathanaelkane@gmail.com>
2023-08-30 21:09:08 +1000

    nvim: Use 2 space indents for markdown

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -8,4 +8,4 @@ vim.cmd [[autocmd FileType gitcommit setlocal spell]]
 vim.cmd [[autocmd TextYankPost * silent! lua vim.highlight.on_yank {higroup = "IncSearch", timeout = 250}]]
 
 -- 2 space indents
-vim.cmd [[autocmd FileType ruby,rails,haml,hamlc,eruby,yaml,ru,cucumber set ai sw=2 sts=2 et]]
+vim.cmd [[autocmd FileType ruby,rails,haml,hamlc,eruby,yaml,ru,cucumber,markdown set ai sw=2 sts=2 et]]
