3c3f0453531dfe0620c58bd111047d250bbcc871
Nate Kane <nathanaelkane@gmail.com>
2025-01-08 16:44:40 +1000

    nvim: Configure flash plugin

--- lazy-vim/lua/plugins/flash.lua
+++ lazy-vim/lua/plugins/flash.lua
@@ -3,34 +3,12 @@ return {
   opts = {
     modes = {
       search = {
-        enabled = false,
+        enabled = true,
       },
       char = {
         enabled = false,
       },
     },
   },
-  keys = function()
-    return {
-      {
-        "<leader>e",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-        desc = "Flash",
-      },
-      {
-        "<leader>j",
-        function()
-          require("flash").jump({
-            search = { mode = "search", max_length = 0 },
-            label = { after = { 0, 0 } },
-            pattern = "^",
-          })
-        end,
-        desc = "Jump To Line",
-      },
-    }
-  end,
+  keys = false,
 }
