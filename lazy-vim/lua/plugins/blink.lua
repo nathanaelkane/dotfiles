9c7b552a2c534531764a35163642041492b14e4b
Nate Kane <nathanaelkane@gmail.com>
2025-01-15 14:53:38 +1000

    nvim: Refactor blink.cmp configuration
    Fixed issue with buffer results being excluded when lsp source was
    enabled.

--- lazy-vim/lua/plugins/blink.lua
+++ lazy-vim/lua/plugins/blink.lua
@@ -1,49 +1,56 @@
-return {
+return  {
   {
     "saghen/blink.cmp",
     dependencies = {
+      "rafamadriz/friendly-snippets",
       "mikavilpas/blink-ripgrep.nvim",
     },
-    opts = function(_, opts)
-      opts.sources.default = { "buffer", "path", "snippets" } -- exclude "lsp" for now as including "lsp" will disable "buffer" as soon as LSP becomes enabled in a buffer
-
-      opts.sources.providers = {
-        ripgrep = {
-          module = "blink-ripgrep",
-          name = "Ripgrep",
-          opts = {
-            prefix_min_len = 3,
-            context_size = 5,
-            max_filesize = "1M",
-            project_root_marker = ".git",
-            search_casing = "--ignore-case",
-            additional_rg_options = {},
-            fallback_to_regex_highlighting = true,
-            debug = false,
-            future_features = {
-              kill_previous_searches = true,
-            },
+    version = "*",
+    opts = {
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      sources = {
+        default = { "lsp", "buffer", "path", "snippets" },
+        providers = {
+          lsp = {
+            fallbacks = {}, -- without this override, buffer results aren't included
           },
-          transform_items = function(_, items)
-            for _, item in ipairs(items) do
-              item.labelDetails = {
-                description = "(rg)",
-              }
-            end
-            return items
-          end,
+          ripgrep = {
+            module = "blink-ripgrep",
+            name = "Ripgrep",
+            opts = {
+              prefix_min_len = 3,
+              context_size = 5,
+              max_filesize = "1M",
+              project_root_marker = ".git",
+              search_casing = "--ignore-case",
+              additional_rg_options = {},
+              fallback_to_regex_highlighting = true,
+              debug = false,
+              future_features = {
+                kill_previous_searches = true,
+              },
+            },
+            transform_items = function(_, items)
+              for _, item in ipairs(items) do
+                item.labelDetails = {
+                  description = "(rg)",
+                }
+              end
+              return items
+            end,
+          },
         },
-      }
-
-      opts.completion.ghost_text.enabled = false
-
-      opts.completion.trigger = opts.completion.trigger or {}
-      opts.completion.trigger.show_in_snippet = false
-
-      opts.completion.list = opts.completion.list or {}
-      opts.completion.list.selection = { preselect = false, auto_insert = true }
-
-      opts.keymap = {
+      },
+      completion = {
+        list = {
+          selection = { preselect = false, auto_insert = true },
+        },
+        trigger = { show_in_snippet = false },
+      },
+      keymap = {
         preset = "super-tab",
         ["<C-y>"] = { "select_and_accept" },
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
@@ -57,17 +64,13 @@ return {
           end,
           "fallback",
         },
-        ["<C-l>"] = {
-          function()
-            require("blink-cmp").show({ providers = { "lsp" } })
-          end,
-        },
         ["<C-r>"] = {
           function()
             require("blink-cmp").show({ providers = { "ripgrep" } })
           end,
         },
-      }
-    end,
-  },
+      },
+    },
+    opts_extend = { "sources.default" },
+  }
 }
