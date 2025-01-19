f8fdd32cb61b01c20edd51c63b6a73dac5a70eeb
Nate Kane <nathanaelkane@gmail.com>
2025-01-19 21:31:31 +1000

    shell: Remove --group-directories-first for aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,4 +1,4 @@
 alias killbg='kill -9 %'
 alias c='clear'
-alias ls='ls -a --color=auto --group-directories-first'
-alias ll='ls -la --color=auto --group-directories-first'
+alias ls='ls -a --color=auto'
+alias ll='ls -la --color=auto'
