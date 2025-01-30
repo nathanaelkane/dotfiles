9b1fc8fa883f0c4cd98329dda9c22f04db033d84
Nate Kane <nathanaelkane@gmail.com>
2025-01-31 09:56:44 +1000

    lazyvim: Use snacks as default picker

--- lazy-vim/lua/config/options.lua
+++ lazy-vim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
 vim.g.autoformat = false
+vim.g.lazyvim_picker = "snacks"
 vim.g.root_spec = { "cwd" } -- prevent LSP and others from changing the root dir
 vim.opt.cursorlineopt = "number"
 vim.opt.relativenumber = false
