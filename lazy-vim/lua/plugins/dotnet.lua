c6a37b7324166665f1657f8cfa77d393ba677e63
Nate Kane <nathanaelkane@gmail.com>
2025-03-31 22:14:39 +1000

    lvim: Override lang.omnisharp extra to not install netcoredbg
    Netcoredbg has no native macOS arm64 build and you need to compile it
    manually.

--- lazy-vim/lua/plugins/dotnet.lua
+++ lazy-vim/lua/plugins/dotnet.lua
@@ -1,5 +1,17 @@
 return {
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      -- netcoredbg has no native macOS arm64 build, have to compile manually
+      for i, v in ipairs(opts.ensure_installed) do
+        if v == "netcoredbg" then
+          table.remove(opts.ensure_installed, i)
+          break
+        end
+      end
+    end,
+  },
+  {
     "nvim-neotest/neotest",
     dependencies = {
       "Issafalcon/neotest-dotnet",
