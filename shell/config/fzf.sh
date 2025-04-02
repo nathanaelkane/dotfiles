0ebf2741b3f5312647ca8be0927441ae4b7063e1
Nate Kane <nathanaelkane@gmail.com>
2025-04-02 10:21:00 +1000

    shell: Set up fzf key bindings and fuzzy completion

--- shell/config/fzf.sh
+++ shell/config/fzf.sh
@@ -1,4 +1,7 @@
 export FZF_DEFAULT_COMMAND='rg --files'
 export FZF_DEFAULT_OPTS='--layout=reverse'
 
+# Set up fzf key bindings and fuzzy completion
+eval "$(fzf --bash)"
+
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
